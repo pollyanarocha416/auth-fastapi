e67585c8de057785779f68d53f67535fd7faf0ee
pollyanarocha416 <pollyanarocha416@gmail.com>
2025-03-05 19:50:09 -0400

    Adicionando user login com jwt

--- app/auth_user.py
+++ app/auth_user.py
@@ -5,6 +5,13 @@ from app.db.schemas import User
 from passlib.context import CryptContext
 from fastapi.exceptions import HTTPException
 from fastapi import status
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from decouple import config
+
+
+SECRET_KEY = config('SECRET_KEY')
+ALGORITHM = config("ALGORITHM")
 
 crypt_context = CryptContext(schemes=["sha256_crypt"])
 
@@ -24,3 +31,32 @@ class UserUseCases:
                 status_code=status.HTTP_400_BAD_REQUEST, 
                 detail="User already exists"
             )
+
+    def user_login(self, user: User, expires_in: int = 30):
+        user_on_db = self.db_session.query(UserModel).filter_by(username=user.username).first()
+
+        if user_on_db is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Invalid username or password'
+            )
+
+        if not crypt_context.verify(user.password, user_on_db.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Invalid username or password'
+            )
+        
+        exp = datetime.utcnow() + timedelta(minutes=expires_in)
+
+        payload = {
+            'sub': user.username,
+            'exp': exp
+        }
+        
+        access_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+        
+        return {
+            'access_token': access_token,
+            'exp': exp.isoformat()        
+        }
--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app.depends import get_db_session
 from app.auth_user import UserUseCases
 from app.db.schemas import User
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter(prefix="/user")
@@ -22,3 +23,24 @@ def user_register(
         content={"msg": "User created"},
         status_code=status.HTTP_201_CREATED
     )
+
+
+@router.post("/login")
+def user_login(
+    request_form_user: OAuth2PasswordRequestForm = Depends(),
+    db_session: Session = Depends(get_db_session)
+):
+
+    uc = UserUseCases(db_session=db_session)
+    user = User(
+        username = request_form_user.username,
+        password = request_form_user.password
+    )
+    
+    auth_data = uc.user_login(user=user)
+    
+    return JSONResponse(
+        content=auth_data,
+        status_code=status.HTTP_200_OK
+    )
+
